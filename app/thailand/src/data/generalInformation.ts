782a9d4de871ad2850cca7de3e01d3d4e5000331
amVictor <amburma@gmail.com>
2026-05-14 11:11:00 +0200

    Fix unterminated string in German general information

--- app/thailand/src/data/generalInformation.ts
+++ app/thailand/src/data/generalInformation.ts
@@ -483,4 +483,6 @@ Besucher können zwischen drei großen Anbietern wählen:
 ### Sicherheit
 
 * **[Do]:** **Wasser aus Flaschen trinken**—*Trinken Sie nur versiegeltes Wasser aus Flaschen oder gefiltertes Wasser.*
+* **[Do]:** **Wasser aus Flaschen trinken**—*Trinken Sie nur versiegeltes Wasser aus Flaschen oder gefiltertes Wasser.*
 * **[Don't]:** **Buddha respektieren**—*Klettern Sie nicht auf Statuen und nutzen Sie Buddha-Abbildungen nicht als Modeaccessoire oder Tattoo.*
+ 
