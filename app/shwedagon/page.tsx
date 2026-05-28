643a050a751dbfec068fa4d184777bcbaa1649e6
amVictor <amburma@gmail.com>
2026-05-28 23:31:18 +0200

    fix DialogClose render prop

--- app/shwedagon/page.tsx
+++ app/shwedagon/page.tsx
@@ -1267,11 +1267,11 @@ function EmergencySection({ t, setActiveSection }: { t: (k: string) => string, s
               </div>
 
               <div className="p-4 border-t border-gold-soft/10 bg-white flex justify-end">
-                <DialogClose render={
-                  <Button variant="ghost" className="rounded-xl text-gray-500 hover:text-gold-deep">
-                    Close
-                  </Button>
-                } />
+                <DialogClose asChild>
+  <Button variant="ghost" className="rounded-xl text-gray-500 hover:text-gray-700">
+    Close
+  </Button>
+</DialogClose>
               </div>
             </DialogContent>
           </Dialog>
