9b1cc7de7989ef09af1c2e4453c0ca76b789183d
amVictor <amburma@gmail.com>
2026-07-04 18:09:24 +0200

    Add Travelpayouts Drive tracking script to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import CookieBanner from '@/components/shared/CookieBanner'
 import { Playfair_Display, Inter, DM_Mono } from 'next/font/google'
 import { cookies } from 'next/headers'
+import Script from "next/script";
 
 const playfair = Playfair_Display({ 
   subsets: ['latin'],
@@ -29,6 +30,20 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang={language} className={`${playfair.variable} ${inter.variable} ${dmMono.variable}`}>
       <head>
+        <Script
+          id="travelpayouts-drive"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                  var script = document.createElement("script");
+                  script.async = 1;
+                  script.src = 'https://emrldtp.com/NTQ2Mzky.js?t=546392';
+                  document.head.appendChild(script);
+              })();
+            `,
+          }}
+        />
         <meta name="google" content="notranslate" />
       </head>
       <body>{children}<CookieBanner /></body>
